edbc3bdd5a6b5c4f1f1f7eb693d6281d3f3b906e
Jaden37 <julespeguet.jp@gmail.com>
2018-12-01 19:55:10 +0100

    Requettes HTTP

---
+++ WpfApp1/WpfApp1/Class/DBConnect.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Web;
+
+namespace WpfApp1.Class
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "ppe2mairie";
+            uid = "root";
+            password = "root";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+            OpenConnection();
+        }
+
+        //open connection to database
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Select statement
+        public int Select()
+        {
+            string query = "SELECT Count(*) FROM club";
+
+            int Count = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+
+
+        //Insert statement
+        public void Insert()
+        {
+        }
+
+        //Update statement
+        public void Update()
+        {
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+        }
+
+        //Select statement
+        //public List<string>[] Select()
+        //{
+        //}
+
+        //Count statement
+        //public int Count()
+        //{
+        //}
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
+        {
+        }
+    }
+
+}
---
+++ WpfApp1/WpfApp1/Class/SecondWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Web;
+
+namespace WpfApp1.Class
+{
+    public class SecondWindow : Window
+    {
+        static Button b1 = new Button();
+        static DockPanel dp1 = new DockPanel();
+        static Label lab = new Label();
+
+        public SecondWindow()
+        {
+            this.Height = 600;
+            this.Width = 1000;
+            this.Hide();
+            this.Closed += Fermer;
+            b1.Content = "Click";
+            b1.Height = 100;
+            b1.Width = 100;
+            dp1.Children.Add(b1);
+            DockPanel.SetDock(b1, Dock.Left);
+            lab.Content = "Coucou c'est moi";
+            dp1.Children.Add(lab);
+            DockPanel.SetDock(lab, Dock.Left);
+            this.Content = dp1;
+        }
+
+        private void Fermer(object sender, EventArgs e)
+        {
+            dp1.Children.Clear();
+            this.Close();
+            MainWindow.SetWindow2();
+        }
+    }
+}
